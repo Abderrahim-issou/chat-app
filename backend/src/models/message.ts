d062f7a4409db14b5a29d96a9c9f712e476bb886
ISSOU <abderrahimissou685@gmail.com>
2026-02-18 17:05:32 +0100

    add necessery models

---
+++ backend/src/models/message.ts
@@ -0,0 +1,56 @@
+import { Document, model, Schema, Types } from "mongoose";
+
+interface IMessage extends Document {
+    receiverId: Types.ObjectId;
+    senderId: Types.ObjectId;
+    type: string;
+    content: string;
+    isSeen: boolean;
+    seenAt: Date;
+    isUpdated: boolean;
+    isDeleted: Types.ObjectId[];
+    replyTo: Types.ObjectId;
+}
+
+const messageSchema = new Schema<IMessage>({
+    receiverId: {
+        type : Schema.Types.ObjectId,
+        required: true
+    },
+    senderId: {
+        type : Schema.Types.ObjectId,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    isSeen: {
+        type: Boolean,
+        required: false
+    },
+    seenAt: {
+        type: Date,
+        required: false
+    },
+    isUpdated: {
+        type: Boolean,
+        required: false
+    },
+    isDeleted: {
+        type: [Schema.Types.ObjectId],
+        required: false, 
+        default: []
+    },
+    replyTo: {
+        type: Schema.Types.ObjectId,
+        required: false
+    },
+});
+
+const Message = model('Message', messageSchema);
+export default Message;
