ba5ce91b609aacc666c15eae2bed8ea01d920226
ISSOU <abderrahimissou685@gmail.com>
2026-03-17 23:55:40 +0100

    set up connection  and disconecting events

--- backend/src/socket/socketEvents.ts
+++ backend/src/socket/socketEvents.ts
@@ -5,21 +5,47 @@ import { getSocketInstance } from "./socket";
 
 
 const Io = getSocketInstance();
-const onlineUsers = new Map<string, number>();
+const onlineUsers = new Map<string, Set<string>>();
 
 
 Io.on('connection', (socket) => {
     const user: currentUser = socket.data.user;
     console.log('User connected :', socket.id);
     
+
+    //check if the user is already connected: 
+    if(!onlineUsers.has(`user:${user.id}`)){
+        onlineUsers.set(`user:${user.id}`,new Set());
+    }
+    onlineUsers.get(`user:${user.id}`).add(socket.id);
+
     //user specific room;
     socket.join(`user:${user.id}`);
 
-    //check if the user is already connected: 
-    const count = onlineUsers.get(`user:${user.id}`) || 0;
-
     // add the user as an online user
-    onlineUsers.set(`user:${user.id}`, count + 1);
+    console.log("User joined:", user.id, "online sockets:", onlineUsers.get(`user:${user.id}`)?.size);
+
+
+
+
+    socket.on('diconnecting', () => {
+        const userId = user.id;
+        if(!userId) return;
+
+        const userSockets = onlineUsers.get(`user:${userId}`);
+        if(userSockets){
+            userSockets.delete(socket.id);
+            if(userSockets.size == 0){
+                onlineUsers.delete(`user:${userId}`);
+                console.log( `user : => user:${userId} just went offline`);
+                Io.emit('user-offline', userId);
+            }else{
+                console.log('user still online');
+            }
+        }
+    });
+
+
 
 
     //send message 
@@ -44,8 +70,12 @@ Io.on('connection', (socket) => {
 
 
     // handle diconenctions
-    socket.on('dieconnect', (reason) => {
+    socket.on('diconnect', (reason) => {
         console.log('disconnete :', reason);
+        const userId = socket.data.user.id
+        if (!userId) return;
+        const userSockets = onlineUsers.get(`user:${user.id}`);
+        if(userSockets) return;
     });
 
 
