8986f7fcc89aa52b185d7866f5e7d6de23b77976
ISSOU <abderrahimissou685@gmail.com>
2026-03-19 15:18:51 +0100

    refactor(frontend): update app entry point

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Provider } from 'react-redux'
+import { store } from './store/index.ts'
+import { BrowserRouter } from 'react-router-dom'
+import SocketProvider from './socket/SocketProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <SocketProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </SocketProvider>
+    </Provider>
   </StrictMode>,
 )
