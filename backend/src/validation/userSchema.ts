194a25d403bcbd3b645936662a0a32c4165fdb03
ISSOU <abderrahimissou685@gmail.com>
2026-03-19 15:18:45 +0100

    feat(validation): update user schema validation

--- backend/src/validation/userSchema.ts
+++ backend/src/validation/userSchema.ts
@@ -5,35 +5,35 @@ import z from "zod";
 export const registerSchema = z.object({
     fullName: z.string(), 
     email: z.string().email(),
-    password: z.string() 
-        .min(8, "Password must be at least 8 characters long")
-        .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
-        .regex(/[a-z]/, "Password must contain at least one lowercase letter")
-        .regex(/[0-9]/, "Password must contain at least one number")
-        .regex(/[@$!%*?&]/, "Password must contain at least one special character (@$!%*?&)"),
-    profilePic: z.string()
+    password: z.string(),
+        // .min(8, "Password must be at least 8 characters long")
+        // .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+        // .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+        // .regex(/[0-9]/, "Password must contain at least one number")
+        // .regex(/[@$!%*?&]/, "Password must contain at least one special character (@$!%*?&)"),
+    profilePic: z.string().optional()
 })
 
 export const loginSchema = z.object({
     email: z.string().email(),
-    password: z.string()
-        .min(8, "Password must be at least 8 characters long")
-        .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
-        .regex(/[a-z]/, "Password must contain at least one lowercase letter")
-        .regex(/[0-9]/, "Password must contain at least one number")
-        .regex(/[@$!%*?&]/, "Password must contain at least one special character (@$!%*?&)")
+    password: z.string(),
+        // .min(8, "Password must be at least 8 characters long")
+        // .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+        // .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+        // .regex(/[0-9]/, "Password must contain at least one number")
+        // .regex(/[@$!%*?&]/, "Password must contain at least one special character (@$!%*?&)")
 });
 
 export const resetPassswordSchema = z.object({
     email: z.string().email()
 });
 export const resetConfirmSchema = z.object({
-    password: z.string() 
-        .min(8, "Password must be at least 8 characters long")
-        .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
-        .regex(/[a-z]/, "Password must contain at least one lowercase letter")
-        .regex(/[0-9]/, "Password must contain at least one number")
-        .regex(/[@$!%*?&]/, "Password must contain at least one special character (@$!%*?&)"),
+    password: z.string(),
+        // .min(8, "Password must be at least 8 characters long")
+        // .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+        // .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+        // .regex(/[0-9]/, "Password must contain at least one number")
+        // .regex(/[@$!%*?&]/, "Password must contain at least one special character (@$!%*?&)"),
     token: z.string()
 });
 
