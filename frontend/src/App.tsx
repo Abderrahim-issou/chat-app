6ecae13dea8a078e03754625148f2555b051a8e2
ISSOU <abderrahimissou685@gmail.com>
2026-03-19 15:18:49 +0100

    feat(frontend): update App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Chat, { User } from './components/chat/Chat';
+import ChatIpnut from './components/chat/ChatIpnut';
+import ChatList, { ChatItem } from './components/chat/ChatList';
+import DropDowns from './components/chat/DropDowns';
+import ProfileTopSec from './components/chat/ProfileTopSec';
+import MessageBuble from './components/UI/MessageBuble';
+// import Nav from './components/nav/nav';
+import Auth from './pages/Auth';
+import Layout from './layouts/Layout';
+import Error from './pages/Error';
+import Profile from './pages/Profile';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import { Route, Routes } from 'react-router-dom';
+import ProtectedRoute from './components/routes/ProtectedRoute';
+// import MessageBuble from './components/UI/MessageBuble';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Routes>
+      <Route element={<Layout/>}>
+        {/* Home Route */}
+        <Route path='/' element={<h1>Home Page</h1>}/>
+        {/* auth pages */}
+        <Route path='/auth' element={<Auth/>}/>
+        
+        {/* chat route */}
+        <Route path='/chat' element={<ProtectedRoute/>}>
+          <Route path='/chat' element={<Auth/>}/>
+        </Route>
+
+        {/* error */}
+        <Route path='*' element={<Error/>}/>
+      </Route>
+    </Routes>
+
     </>
   )
 }
